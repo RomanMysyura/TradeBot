9f326494c3775626f17378d9bfacb6322a193cec
RomanMysyura <rmysyura@cendrassos.net>
2024-06-04 00:30:23 +0200

    Beta version 1.0.1

--- bot.mjs
+++ bot.mjs
@@ -2,80 +2,79 @@ import fetch from 'node-fetch';
 import chalk from 'chalk';
 import Table from 'cli-table3';
 
-let transactions = []; // Registro de transacciones
-let currentTransaction = null; // Transacción actual en proceso
-const precios = [];
-const longSmaPeriod = 50; // Periodo de la media móvil lenta
-const shortSmaPeriod = 20; // Periodo de la media móvil rápida
-let ultimaSeñal = null;
-let balance = 30; // Balance principal
-let precioCompra = 0;
-let precioVenta = 0;
-let profitTotal = 0;
-let comprado = false; // Estado de compra
+let transactions = []; // Transaction records
+let currentTransaction = null; // Current transaction in progress
+const prices = [];
+const longSmaPeriod = 50; // Slow moving average period
+const shortSmaPeriod = 20; // Fast moving average period
+let lastSignal = null;
+let balance = 30; // Initial principal balance
+let purchasePrice = 0;
+let sellPrice = 0;
+let totalProfit = 0;
+let bought = false; // Purchase status
 
 let table = createTable();
 
-async function obtenerPrecioBitcoin() {
+async function fetchBitcoinPrice() {
   try {
     const response = await fetch('https://api.binance.com/api/v3/avgPrice?symbol=BTCUSDT');
     const data = await response.json();
-    const precio = parseFloat(data.price);
+    const price = parseFloat(data.price);
 
-    precios.push(precio);
+    prices.push(price);
 
-    if (!comprado) {
-      if (precios.length >= longSmaPeriod) {
-        const shortSma = calcularSma(precios, shortSmaPeriod);
-        const longSma = calcularSma(precios, longSmaPeriod);
+    if (!bought) {
+      if (prices.length >= longSmaPeriod) {
+        const shortSma = calculateSma(prices, shortSmaPeriod);
+        const longSma = calculateSma(prices, longSmaPeriod);
 
         if (shortSma && longSma) {
-          const señal = determinarSeñal(shortSma, longSma, precio);
-          actualizarTabla(precio, señal);
+          const signal = determineSignal(shortSma, longSma, price);
+          updateTable(price, signal);
         }
       } else {
-        actualizarTabla(precio, null);
+        updateTable(price, null);
       }
     } else {
-      const porcentajeVariacion = ((precio - precioCompra) / precioCompra) * 100;
-      if (porcentajeVariacion <= -0.001 || porcentajeVariacion >= 0.001) {
-        precioVenta = precio;
-        const profit = precioVenta - precioCompra;
-        profitTotal += profit;
-        console.log(chalk.red(`Señal de VENTA a: ${precioVenta} USDT`));
-        console.log(chalk.yellow(`Profit de esta operación: ${profit} USDT`));
-        console.log(chalk.yellow(`Profit total: ${profitTotal} USDT`));
-        currentTransaction.venta = precioVenta;
+      const percentageChange = ((price - purchasePrice) / purchasePrice) * 100;
+      if (percentageChange <= -0.001 || percentageChange >= 0.001) {
+        sellPrice = price;
+        const profit = sellPrice - purchasePrice;
+        totalProfit += profit;
+        console.log(chalk.red(`SELL signal at: ${sellPrice} USDT`));
+        console.log(chalk.yellow(`Profit for this transaction: ${profit} USDT`));
+        console.log(chalk.yellow(`Total profit: ${totalProfit} USDT`));
+        currentTransaction.sell = sellPrice;
         currentTransaction.profit = profit;
         transactions.push(currentTransaction);
-        comprado = false;
-        if (transactions.length > 1) { // Solo ajusta el balance si no es la primera transacción
-          balance += (profit > 0) ? profit : 0;
-        }
-        resetearTabla();
+        bought = false;
+        balance += profit; // Adjust balance based on profit/loss
+        resetTransaction(); // Reset transaction details for the next transaction
+        resetEverythingExceptTransactionRecords(); // Reset everything except transaction records
       } else {
-        actualizarTabla(precio);
+        updateTable(price);
       }
     }
   } catch (error) {
-    console.error('Error al obtener el precio:', error);
+    console.error('Error fetching price:', error);
   }
 }
 
 function createTable() {
   return new Table({
     head: [
-      chalk.cyan('Precio Actual de Bitcoin'),
-      chalk.green('Precio de Compra'),
-      chalk.red('Precio de Venta'),
-      chalk.yellow('Profit Total'),
-      chalk.blue('Balance Total')
+      chalk.cyan('Current Bitcoin Price'),
+      chalk.green('Purchase Price'),
+      chalk.red('Sell Price'),
+      chalk.yellow('Total Profit'),
+      chalk.blue('Total Balance')
     ],
     colWidths: [30, 30, 30, 30, 30]
   });
 }
 
-function calcularSma(data, period) {
+function calculateSma(data, period) {
   if (data.length < period) {
     return null;
   }
@@ -84,29 +83,32 @@ function calcularSma(data, period) {
   return sum / period;
 }
 
-function determinarSeñal(shortSma, longSma, precioActual) {
-  if (shortSma > longSma && ultimaSeñal !== 'comprar' && balance >= 10) {
-    ultimaSeñal = 'comprar';
-    precioCompra = precioActual;
-    console.log(chalk.green(`Señal de COMPRA a: ${precioCompra} USDT`));
-    comprado = true;
+function determineSignal(shortSma, longSma, currentPrice) {
+  if (shortSma > longSma && lastSignal !== 'buy' && balance >= 10) {
+    lastSignal = 'buy';
+    purchasePrice = currentPrice;
+    console.log(chalk.green(`BUY signal at: ${purchasePrice} USDT`));
+    bought = true;
     currentTransaction = {
-      compra: precioCompra,
-      venta: null,
+      buy: purchasePrice,
+      sell: null,
       profit: null
     };
-    return 'comprar';
+    // Realizar la compra por una cantidad fija de $10
+    balance -= 10; // Restar $10 del balance
+    console.log(chalk.yellow(`Balance after purchase: ${balance} USDT`));
+    return 'buy';
   }
   return null;
 }
 
-function actualizarTabla(precioActual, señal) {
+function updateTable(currentPrice, signal) {
   const newTable = createTable();
   newTable.push([
-    precioActual.toFixed(2) + ' USDT',
-    precioCompra ? precioCompra.toFixed(2) + ' USDT' : '-',
-    precioVenta ? precioVenta.toFixed(2) + ' USDT' : '-',
-    profitTotal.toFixed(2) + ' USDT',
+    currentPrice.toFixed(2) + ' USDT',
+    purchasePrice ? purchasePrice.toFixed(2) + ' USDT' : '-',
+    sellPrice ? sellPrice.toFixed(2) + ' USDT' : '-',
+    totalProfit.toFixed(2) + ' USDT',
     balance.toFixed(2) + ' USDT'
   ]);
 
@@ -114,20 +116,26 @@ function actualizarTabla(precioActual, señal) {
   console.log(newTable.toString());
 
   if (transactions.length > 0) {
-    console.log('\nRegistro de Transacciones:');
+    console.log('\nTransaction Records:');
     console.table(transactions);
-    const totalProfit = transactions.reduce((total, transaction) => total + transaction.profit, 0);
-    console.log(chalk.yellow(`Profit Total de Todas las Transacciones: ${totalProfit.toFixed(2)} USDT`));
+    const totalTransactionProfit = transactions.reduce((total, transaction) => total + transaction.profit, 0);
+    console.log(chalk.yellow(`Total Profit from All Transactions: ${totalTransactionProfit.toFixed(2)} USDT`));
   }
 
   table = newTable;
 }
 
-function resetearTabla() {
-  precioCompra = 0;
-  precioVenta = 0;
-  profitTotal = 0.00 + ' USDT';
-  table = createTable();
+function resetTransaction() {
+  currentTransaction = null;
+  purchasePrice = 0;
+  sellPrice = 0;
 }
 
-setInterval(obtenerPrecioBitcoin, 250);
+function resetEverythingExceptTransactionRecords() {
+  prices.length = 0;
+  lastSignal = null;
+  balance = 30;
+  totalProfit = 0;
+}
+
+setInterval(fetchBitcoinPrice, 250);
